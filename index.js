d0c16dbcb30c856cf2cf18b2dece335eb25f410e
Wenbo Wang <wwenbow@gmail.com>
2016-04-28 16:44:08 -0700

    dump csv in cassandra and download omdb as psv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const http = require('http');
 const cassandra = require('cassandra-driver');
 
-const dataDir = process.env.HOME + '/Downloads/ml-latest-small';
+const dataDir = process.env.HOME + '/Downloads/ml-20m';
 const cassandraNode = 'default-machine';
 const keyspace = 'test';
 const client = new cassandra.Client({ contactPoints: [cassandraNode], keyspace: keyspace});
@@ -35,6 +35,37 @@ Response: 'True' }
 */
 
 /**
+ * Format data to the correct types
+ */
+var dataFormater = {
+    movies : function(text){
+        // movieId, title, genres
+        return [ parseInt(text[0]), text[1], text[2] ];
+    },
+    ratings: function(text){
+        // userId, movieId, rating
+        return [ parseInt(text[0]), parseInt(text[1]), parseFloat(text[2]) ];
+    },
+    tags : function(text){
+        // userId, movieId, tag
+        return [ parseInt(text[0]), parseInt(text[1]), text[2] ];
+    },
+    links : function(text){
+        // movieId, imdb, tmdb
+        return [ parseInt(text[0]), text[1], text[2] ];
+    },
+    imdb : function(text){
+        var ret = text;
+        ret[0] = parseInt[ret[0]];
+        ret[2] = parseInt[ret[2]];
+        ret[16] = parseFloat[ret[16]];
+        ret[17] = parseFloat[ret[17]];
+        ret[18] = parseInt[ret[18]];
+        return ret;
+    }
+};
+
+/**
  * Execute a list of queries and return an array of promises
  * queries = [
  * {
@@ -44,15 +75,17 @@ Response: 'True' }
  *  ...
  *  ]
  */
-function executeQueries(queries){
+function executeQueries(queries, options, log){
     return queries.map(function(q){
         return new Promise(function(resolve, reject) {
-            client.execute(q.query, q.params, function(err, result) {
+            client.execute(q.query, q.params, options, function(err, result) {
                 if (err) {
-                    console.log(err);
+                    console.error(err);
                     return reject(err);
                 }
-                console.log('completed: ' + q.query);
+                if (log) {
+                    console.log('completed: ' + q.query);
+                }
                 resolve();
             });
         });
@@ -64,36 +97,68 @@ function executeQueries(queries){
  *
  * returns an array of promises
  */
-function loadCassandraWithCSV(tables){
+function loadCassandraWithCSV(tables, schemas, delimiter, header, filetype){
     return tables.map(function(table) {
         return new Promise(function(resolve, reject) {
-            fs.readFile(dataDir + '/' + table + '.csv', function read(err, data) {
+            fs.readFile(dataDir + '/' + table + filetype, function read(err, data) {
                 if (err) {
                     console.log(err);
                     return reject(err);
                 }
-                csv.parse(data, {auto_parse: true}, function(err, output) {
+                csv.parse(data, {delimiter : delimiter}, function(err, output) {
                     if (err) {
                         console.log(err);
                         return reject(err);
                     }
-                    var csvdata = output.splice(1); //remove header
+                    var csvdata = [];
+                    if (header) {
+                        csvdata = output.splice(1); //remove header
+                    } else {
+                        csvdata = output;
+                    }
+
+                    var insertQueries = [];
+                    var schema = schemas[table];
+                    var ncols = schema.split(',').length;
+                    var questionMarks = Array(ncols).fill('?').join(',');
+                    questionMarks = '(' + questionMarks + ')';
+
+                    for (var i = 0; i < csvdata.length; i++){
+                        var insertQuery = {
+                            query : 'INSERT INTO ' + table + ' ' + schema + ' VALUES' + questionMarks + ';',
+                            params : dataFormater[table](csvdata[i])
+                        };
+                        insertQueries.push(insertQuery);
+                    }
+
+                    console.log('csv loaded ' + table);
+
+                    Promise.all(executeQueries(insertQueries, { prepare: true },false))
+                    .then(function(){
+                        console.log('inserted ' + table);
+                        resolve();
+                    })
+                    .catch(function(err){
+                        console.error(err);
+                        reject(err);
+                    });
                 });
             });
         });
     });
 }
 
+
 var main = function(){
     const args = process.argv.slice(2);
 
-    const tables = ['ratingsb', 'tagsb', 'moviesb', 'linksb', 'imdbb'];
-    const schemas = [
-        '(user int, movie int, rating double, PRIMARY KEY(user, movie))', //rating
-        '(user int, movie int, tag varchar, PRIMARY KEY(user, movie))', //tag
-        '(movie int PRIMARY KEY, name varchar, genres varchar)', //movie
-        '(movie int PRIMARY KEY, imdb varchar, tmdb varchar)', //link
-        ['(movie int PRIMARY KEY,', //imdb
+    const tables = ['ratings', 'tags', 'movies', 'links', 'imdb'];
+    const schemas = {
+        ratings : '(user int, movie int, rating double, PRIMARY KEY(user, movie))', //rating
+        tags : '(user int, movie int, tag varchar, PRIMARY KEY(user, movie))', //tag
+        movies : '(movie int PRIMARY KEY, name varchar, genres varchar)', //movie
+        links : '(movie int PRIMARY KEY, imdb varchar, tmdb varchar)', //link
+        imdb : ['(movie int PRIMARY KEY,', //imdb
             'imdb varchar,',
             'title varchar,',
             'year int,',
@@ -114,13 +179,41 @@ var main = function(){
             'imdbvotes int,',
             'imdbid varchar,',
             'type varchar)'].join(' ')
-    ];
+    };
+
+    const colNames = {
+        ratings : '(user, movie, rating)', //rating
+        tags : '(user, movie, tag)', //tag
+        movies : '(movie, name, genres)', //movie
+        links : '(movie, imdb, tmdb)', //link
+        imdb : ['(movie,', //imdb
+            'imdb,',
+            'title,',
+            'year,',
+            'rated,',
+            'released,',
+            'runtime,',
+            'genre,',
+            'director,',
+            'writer,',
+            'actors,',
+            'plot,',
+            'language,',
+            'country,',
+            'awards,',
+            'poster,',
+            'metascore,',
+            'imdbrating,',
+            'imdbvotes,',
+            'imdbid,',
+            'type)'].join(' ')
+    };
 
     var dropQueries = [];
     var createQueries = [];
     for (var i = 0; i < tables.length; i++) {
         var table = tables[i];
-        var schema = schemas[i];
+        var schema = schemas[table];
         if (args[0] == 'drop') {
             dropQueries.push({
                 query: 'DROP TABLE IF EXISTS ' + table
@@ -132,123 +225,29 @@ var main = function(){
     }
 
 
-    Promise.all(executeQueries(dropQueries))
+    Promise.all(executeQueries(dropQueries, {}, true))
     .then(function() {
         console.log('all dropped');
-        return Promise.all(executeQueries(createQueries));
+        return Promise.all(executeQueries(createQueries, {}, true));
     })
     .then(function() {
         console.log('all created');
-        return Promise.all(loadCassandraWithCSV(['movies']));
+        return Promise.all(loadCassandraWithCSV(tables.splice(0, 4),
+                                                colNames, ',', true, '.csv'));
+    })
+    .then(function() {
+        console.log('all created');
+        return Promise.all(loadCassandraWithCSV([ 'imdb' ] ,
+                                                colNames, '|', false, '.psv'));
     })
     .then(function() {
         console.log('all loaded');
         process.exit(0);
     })
-    .catch(console.error);
-
-    /*
-       const insertQuery = 'INSERT INTO imdb (movie, imdb, title, year, rated, '
-       + 'released, runtime, genre, director, writer, actors, plot, language, '
-       + 'country, awards, poster, metascore, imdbrating, imdbvotes, imdbid, '
-       + 'type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
-
-       const query = 'SELECT movie, imdb FROM links';
-       const backupDir = __dirname + '/backup'
-
-       var count = 0;
-       var callbacks = 0;
-
-    //Limit http connections to avoid connection errors
-    http.globalAgent.maxSockets = 50;
-
-    //create table
-    client.execute(creatQuery,
-    function (err, res) {
-    if(err) {
-    console.info(err);
-    }
-
-    client.eachRow(query, [],
-    function(n, row) {
-    count += 1;
-    callbacks += 2;
-
-    http.get({
-host: 'www.omdbapi.com',
-path: '/?i=tt' + row.imdb
-},
-function(response) {
-var body = '';
-response.on('data', function(d) {
-body += d;
-});
-response.on('end', function() {
-const parsed = JSON.parse(body);
-
-var metascore = parseFloat(parsed.Metascore);
-var imdbrating = parseFloat(parsed.imdbRating);
-var imdbvotes = parseInt(parsed.imdbVotes.replace(',',''))
-
-if (isNaN(metascore)) metascore = -1;
-if (isNaN(imdbrating)) imdbrating = -1;
-if (isNaN(imdbvotes)) imdbvotes = -1;
-
-client.execute(insertQuery,
-    [parseInt(row.movie), row.imdb, parsed.Title, parseInt(parsed.Year), parsed.Rated,
-        parsed.Released, parsed.Runtime, parsed.Genre, parsed.Director, parsed.Writer,
-        parsed.Actors, parsed.Plot, parsed.Language, parsed.Country, parsed.Awards,
-        parsed.Poster, metascore, imdbrating,
-        imdbvotes, parsed.imdbID, parsed.Type],
-        {prepare: true},
-        function (err, res) {
-            if(err) {
-                console.info('movie tt' + row.imdb + ' failed to insert into cassandra');
-                console.info(err);
-            }
-            callbacks -= 1;
-            if (callbacks == 0) {
-                console.log('callbacks completed')
-                    process.exit();
-            }
-        });
-
-        fs.mkdir(backupDir, function () {
-                const file = path.resolve(backupDir + '/tt' + row.imdb);
-                fs.unlink(file, function () {
-                        fs.writeFile(file, JSON.stringify(parsed, null, 2), function(err) {
-                                if(err) {
-                                console.info('movie tt' + row.imdb + ' failed to write to file');
-                                console.info(err);
-                                }
-                                callbacks -= 1;
-                                if (callbacks == 0) {
-                                console.log('callbacks completed')
-                                process.exit();
-                                }
-                                });
-                        });
-                });
-});
-});
-},
-    function (err, result) {
-        if(err) {
-            console.info(err);
-        }
-        if (typeof result.nextPage == "function") {
-            result.nextPage();
-        }
-        else {
-            console.log('processed ' + count + ' rows')
-        }
-    }
-);
-}
-);
-*/
-
-
+    .catch(function(err) {
+        console.error(err);
+        process.exit(1);
+    });
 };
 
 if (require.main === module) {
